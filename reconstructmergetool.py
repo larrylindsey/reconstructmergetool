c57f4ceafceec0df6dfc2111c5cdb97c91837546
Michael Musslewhite <mdmusslewhite@gmail.com>
2013-03-13 12:39:36 -0500

    Updated with reconObjects.py

--- reconstructmergetool.py
+++ reconstructmergetool.py
@@ -6,22 +6,22 @@
 #
 #  Date Created: 3/7/2013
 #
-#  Date Last Modified: 3/12/2013
+#  Date Last Modified: 3/13/2013
 
 from reconObjects import *
 
 path = '/home/michaelm/Documents/reconstructmergetool/testing.xml' #---------
 def main():
     # Create list of Transform objects
-    transList = TransformList()
-    transList.addTfromFile(path)
+    transList = ObjectList()
+    transList.addOfromFile(path, 'transform')
     print('Created Transform List with '+str(len(transList))+' Transform objects.')
     for i in range( len(transList) ):
         print( transList[i] )
         
     # Create list of Contour objects
-    contList = ContourList()
-    contList.addCfromFile(path)
+    contList = ObjectList()
+    contList.addOfromFile(path, 'contour')
     print('Created Contour List with '+str(len(contList))+' Contour objects.')
     for i in range( len(contList) ):
         print( contList[i] )
