4e0f6846dd6ecd390385fb3cc643c7ae110c5ef5
Michael Musslewhite <mdmusslewhite@gmail.com>
2013-07-22 16:18:32 -0500

    removed a few unnecessary files

--- testinggui.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'untitled.ui'
-#
-# Created: Fri Jul 19 12:31:19 2013
-#      by: PyQt4 UI code generator 4.10.2-snapshot-a8a14dd99d1e
-#
-# WARNING! All changes made in this file will be lost!
-
