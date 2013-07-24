e08ccd8b895643beb7e2d06bc4468597fdbeacf4
Michael Musslewhite <mdmusslewhite@gmail.com>
2013-07-24 14:23:36 -0500

    Added some QObject.connect() functionality

--- rmtui.py
+++ rmtui.py
@@ -2,71 +2,62 @@
 
 # Form implementation generated from reading ui file 'qtrmtui.ui'
 #
-# Created: Tue Jul 23 15:05:02 2013
+# Created: Wed Jul 24 11:58:26 2013
 #      by: pyside-uic 0.2.14 running on PySide 1.1.2
 #
 # WARNING! All changes made in this file will be lost!
 
-# Last update: 7/23/2013
-# Changed Ui_Form(object) to Ui_Form(QtGui.QMainWindow) so that self.show() works
-import sys
 from PySide import QtCore, QtGui
 
-
-class Ui_Form(QtGui.QMainWindow):
+class Ui_Form(object):
     def setupUi(self, Form):
         Form.setObjectName("Form")
-        Form.resize(579, 441)
+        Form.resize(579, 440)
         
-        # Layout
-        self.horizontalLayoutWidget = QtGui.QWidget(Form)
-        self.horizontalLayoutWidget.setGeometry(QtCore.QRect(69, 140, 441, 80))
+        # FRAME
+        self.frame = QtGui.QFrame(Form)
+        self.frame.setGeometry(QtCore.QRect(10, 10, 561, 421))
+        self.frame.setFrameShape(QtGui.QFrame.StyledPanel)
+        self.frame.setFrameShadow(QtGui.QFrame.Raised)
+        self.frame.setObjectName("frame")
+
+        # LAYOUT
+        self.horizontalLayoutWidget = QtGui.QWidget(self.frame)
+        self.horizontalLayoutWidget.setGeometry(QtCore.QRect(59, 90, 441, 80))
         self.horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
         self.horizontalLayout = QtGui.QHBoxLayout(self.horizontalLayoutWidget)
-        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+    #     self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
         self.horizontalLayout.setObjectName("horizontalLayout")
         
-        # Load series 1 button
-        self.pushButton = QtGui.QPushButton(self.horizontalLayoutWidget)
-        self.pushButton.setObjectName("pushButton")
-        self.ser1path = self.pushButton.clicked.connect(self.loadSeries)
-        self.horizontalLayout.addWidget(self.pushButton)
+        # LOAD SERIES 1
+        self.ser1Button = QtGui.QPushButton(self.horizontalLayoutWidget)
+        self.ser1Button.setObjectName("ser1Button")
+        self.horizontalLayout.addWidget(self.ser1Button)
         
-        # Load series 2 button
-        self.pushButton_2 = QtGui.QPushButton(self.horizontalLayoutWidget)
-        self.pushButton_2.setObjectName("pushButton_2")
-        self.ser2path = self.pushButton_2.clicked.connect(self.loadSeries)
-        self.horizontalLayout.addWidget(self.pushButton_2)
+        # LOAD SERIES 2
+        self.ser2Button = QtGui.QPushButton(self.horizontalLayoutWidget)
+        self.ser2Button.setObjectName("ser2Button")
+        self.horizontalLayout.addWidget(self.ser2Button)
+            
+        # QUIT
+        self.quitButton = QtGui.QPushButton(self.frame)
+        self.quitButton.setGeometry(QtCore.QRect(460, 390, 96, 27))
+        self.quitButton.setObjectName("quitButton")
         
-        # Start merge button
-        self.pushButton_3 = QtGui.QPushButton(Form)
-        self.pushButton_3.setGeometry(QtCore.QRect(210, 300, 161, 91))
-        self.pushButton_3.setObjectName("pushButton_3")
-
+        # START
+        self.startButton = QtGui.QPushButton(self.frame)
+        self.startButton.setGeometry(QtCore.QRect(200, 250, 161, 91))
+        self.startButton.setObjectName("startButton")
+    
         self.retranslateUi(Form)
+        QtCore.QObject.connect(self.quitButton, QtCore.SIGNAL("clicked()"), Form.close)
+        QtCore.QObject.connect(self.ser1Button, QtCore.QAbstract) #=== file dialog goes here
         QtCore.QMetaObject.connectSlotsByName(Form)
-        
+
     def retranslateUi(self, Form):
         Form.setWindowTitle(QtGui.QApplication.translate("Form", "Form", None, QtGui.QApplication.UnicodeUTF8))
-        self.pushButton_3.setText(QtGui.QApplication.translate("Form", "START MERGE", None, QtGui.QApplication.UnicodeUTF8))
-        self.pushButton.setText(QtGui.QApplication.translate("Form", "Load Series 1", None, QtGui.QApplication.UnicodeUTF8))
-        self.pushButton_2.setText(QtGui.QApplication.translate("Form", "Load Series 2", None, QtGui.QApplication.UnicodeUTF8))
-    
-    def loadSeries(self):
-        dialog = QtGui.QFileDialog(self)
-        dialog.setFileMode(QtGui.QFileDialog.AnyFile)
-        dialog.setNameFilter('Series files (*.ser)')
-        dialog.setViewMode(QtGui.QFileDialog.Detail)
-        if dialog.exec_():
-            seriesPath = dialog.selectedFiles()
-            return seriesPath
-        else:
-            return None
-        
-def main():
-    app = QtGui.QApplication(sys.argv)
-    win = Ui_Form()
-    win.show()
-    sys.exit( app.exec_() )
-if __name__ == '__main__':
-    main()
+        self.ser1Button.setText(QtGui.QApplication.translate("Form", "Load Series 1", None, QtGui.QApplication.UnicodeUTF8))
+        self.ser2Button.setText(QtGui.QApplication.translate("Form", "Load Series 2", None, QtGui.QApplication.UnicodeUTF8))
+        self.quitButton.setText(QtGui.QApplication.translate("Form", "Quit :(", None, QtGui.QApplication.UnicodeUTF8))
+        self.startButton.setText(QtGui.QApplication.translate("Form", "START MERGE", None, QtGui.QApplication.UnicodeUTF8))
+
