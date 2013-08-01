b591fdb07f8036a433ad6fc5a17a79e11264b53c
wtrdrnkr <mdmusslewhite@gmail.com>
2013-08-01 14:55:53 -0500

    playing with gui stuff

--- rmtgui.py
+++ rmtgui.py
@@ -7,10 +7,10 @@ import reconstructmergetool as rmt
 # To do: Resize image, show right location, draw contour
 #        Proper feedback when clicking buttons
 
-class RmtGui(QtGui.QMainWindow):
+class loadWindow(QtGui.QWidget):
     
-    def __init__(self):
-        super(RmtGui, self).__init__()
+    def __init__(self, parent=None):
+        QtGui.QWidget.__init__(self, parent)
         self.startUI()
         
     def startUI(self):
@@ -74,38 +74,7 @@ class RmtGui(QtGui.QMainWindow):
             return seriesPath
         else:
             return None
-    
-    def center(self):
-        '''Center a window'''
-        qr = self.frameGeometry()
-        cp = QtGui.QDesktopWidget().availableGeometry().center()
-        qr.moveCenter(cp)
-        self.move(qr.topLeft())
-        
-    def contConf(self,c1,c2,img):
-        # Left/Right Merge Button
-        btn = QtGui.QPushButton('Contour 1', self)
-        btn.setToolTip( str(c1) )
-        btn.resize(btn.sizeHint())
-        btn.move( 200-(85/2), h-(h/2)+150 ) # how far from topleft corner (x, y)
-        
-        btn2 = QtGui.QPushButton('Contour 2', self)
-        btn2.setToolTip( str(c2) )
-        btn2.resize(btn2.sizeHint())
-        btn2.move( 550-(85/2) , h-(h/2)+150 ) 
-        
-        # Center merge button
-        btn3 = QtGui.QPushButton('Output both', self)
-        btn3.setToolTip('Click to output both options')
-        btn3.resize(btn3.sizeHint())
-        btn3.move( (w/2)-(btn3.width()/2), h-(h/2)+200 )
-        
-        # Show image/traces
-        self.picbox(300,300,50,50,c1,img)
-        self.picbox(300,300, w-350, 50,c2,img)
-    
-        
-        
+
     def picbox(self, w, h, x, y,c,img):
         '''w = width of main window
            h = height of main window
@@ -124,10 +93,61 @@ class RmtGui(QtGui.QMainWindow):
         picbox.resize(w,h)
         picbox.move(x,y)
         picbox.setToolTip( str(c) )
+
+class serAttWindow(QtGui.QWidget):   
+    def __init__(self, parent=None):
+        QtGui.QWidget.__init__(self, parent)
+        self.startUI()
+        
+    def startUI(self):
+        # FRAME
+        self.frame = QtGui.QFrame()
+        self.frame.setGeometry(QtCore.QRect(10, 10, 561, 421))
+        self.frame.setFrameShape(QtGui.QFrame.StyledPanel)
+        self.frame.setFrameShadow(QtGui.QFrame.Raised)
+        self.frame.setObjectName("frame")
+
+        # LAYOUT
+        self.horizontalLayoutWidget = QtGui.QWidget(self.frame)
+        self.horizontalLayoutWidget.setGeometry(QtCore.QRect(59, 90, 441, 80))
+        self.horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
+        self.horizontalLayout = QtGui.QHBoxLayout(self.horizontalLayoutWidget)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        
+        # LOAD SERIES 1
+        self.ser1Button = QtGui.QPushButton(self.horizontalLayoutWidget)
+        self.ser1Button.setObjectName("ser1Button")
+        self.horizontalLayout.addWidget(self.ser1Button)
+        self.ser1Button.setText(QtGui.QApplication.translate("Form", "Load Series 1", None, QtGui.QApplication.UnicodeUTF8))
+        self.ser1path = self.ser1Button.clicked.connect(self.loadSeries)
+        
+        # LOAD SERIES 2
+        self.ser2Button = QtGui.QPushButton(self.horizontalLayoutWidget)
+        self.ser2Button.setObjectName("ser2Button")
+        self.horizontalLayout.addWidget(self.ser2Button)
+        self.ser2Button.setText(QtGui.QApplication.translate("Form", "Load Series 2", None, QtGui.QApplication.UnicodeUTF8))
+        self.ser2path = self.ser2Button.clicked.connect(self.loadSeries)
+        
+        # QUIT
+        self.quitButton = QtGui.QPushButton(self.frame)
+        self.quitButton.setGeometry(QtCore.QRect(460, 390, 96, 27))
+        self.quitButton.setObjectName("quitButton")
+        self.quitButton.clicked.connect(QtCore.QCoreApplication.instance().quit)
+        self.quitButton.setText(QtGui.QApplication.translate("Form", "Quit :(", None, QtGui.QApplication.UnicodeUTF8))
+        
+        # START BUTTON
+        self.startButton = QtGui.QPushButton(self.frame)
+        self.startButton.setGeometry(QtCore.QRect(200, 250, 161, 91))
+        self.startButton.setObjectName("startButton")
+        self.startButton.clicked.connect(self.beginMerge)
+        self.startButton.setText(QtGui.QApplication.translate("Form", "START MERGE", None, QtGui.QApplication.UnicodeUTF8))
+        
+        self.frame.show()
+        
         
 def main():
     app = QtGui.QApplication(sys.argv)
-    win = RmtGui()
+    win = loadWindow()
     sys.exit( app.exec_() )
 
 if __name__ == '__main__':
