f0df381388a2d2ecfe516eb7bdf98c7bd05a44a5
wtrdrnkr <mdmusslewhite@gmail.com>
2013-08-16 15:00:31 -0500

    Started reorganizing rmtGui: making widgets references the mainFrame() instead of beings a chain of events. This should help store all important data in the mainFrame() object instead of passing them in between widgets etc

--- rmtgui.py
+++ rmtgui.py
@@ -4,7 +4,7 @@
 import sys
 import reconstructmergetool as rmt
 from PySide import QtCore, QtGui
-import time
+
 # Follows the same steps as reconstructmergetool.mergeSeries(etc) but doesnt call that function...
 # ... easier to implement separately for GUI
 class mainFrame(QtGui.QFrame):
@@ -22,6 +22,11 @@ class mainFrame(QtGui.QFrame):
         self.qButton.clicked.connect(QtCore.QCoreApplication.instance().quit)
         
         self.show()
+        self.rmtStart()
+         
+    def rmtStart(self): #===
+        '''This function begins the process of merging series'''
+        welcomeButton(self)
         
 class welcomeButton(QtGui.QPushButton):
     def __init__(self, parent=None):
@@ -93,7 +98,47 @@ class loadSeries(QtGui.QWidget):
             error = QtGui.QMessageBox(self)
             error.setText('Please load valid .ser into each slot!')
             error.exec_()
-            
+
+class seriesAttributeTable(QtGui.QTableWidget):
+    '''Receives a parent frame, series1 attributes and series2 attributes. Provides a table display
+    GUI for selecting and outputting the attributes to be included in the merged series'''
+    def __init__(self, parent=None, s1atts=None, s2atts=None):
+        QtGui.QTableWidget.__init__(self, len(s1atts), 2, parent=parent)
+        self.parent = parent
+        
+        # Create table
+        self.setColumnWidth(0, 318)
+        self.setColumnWidth(1, 318)
+        self.setGeometry(0,0,800,500)
+        
+        # Make attribute labels for rows, series names for columns
+        self.setHorizontalHeaderLabels( [self.ser1.name, self.ser2.name] ) #==== namess
+        self.setVerticalHeaderLabels( [attribute for attribute in self.ser1._attribs] )
+        
+        # Some rows need to be taller to show all output
+        self.setWordWrap(True)
+        self.resizeRowsToContents()
+        self.setRowHeight(55, 100) # borderColors
+        self.setRowHeight(56, 100) # fillcolors
+        self.setRowHeight(83, 75) # mvmtIncrement
+        self.setRowHeight(84, 75) # ctrlIncrement
+        self.setRowHeight(85, 75) # shiftIncrement
+        
+        # Load table with attributes
+        for row in range(len(s1atts)):
+            header = self.verticalHeaderItem(row).text()
+            # Series 1
+            tableItem = QtGui.QTableWidgetItem( s1atts[header] )
+            self.setItem(row,0,tableItem)
+            # Series 2
+            tableItem = QtGui.QTableWidgetItem( s2atts[header] )
+            self.setItem(row,1,tableItem)
+        
+        self.show()
+        
+    def returnItems(self):
+        return self.selectedItems()
+        
 class seriesAttributes(QtGui.QWidget):
     def __init__(self, parent=None, s1p=None, s2p=None):
         QtGui.QWidget.__init__(self, parent)
@@ -102,6 +147,8 @@ class seriesAttributes(QtGui.QWidget):
         # Frame
         self.frame = QtGui.QFrame(parent)
         self.frame.setGeometry(0,0,800,500)
+        #=== question mark box with instructions?
+        #=== merge button
         
         # Create series objects from paths
         self.ser1 = rmt.getSeries(s1p)
@@ -111,40 +158,34 @@ class seriesAttributes(QtGui.QWidget):
         ser1atts = self.ser1.output()[0]
         ser2atts = self.ser2.output()[0]
         
-        # Create 3 column table: left column has name of all attributes,
-        # middle column is all ser1 atts, right column is all ser2 atts.
-        # Highlight same atts in green for both rows
-        # Rest can only be picked from 1 column
-        table = QtGui.QTableWidget(len(ser1atts), 2, parent=self.frame)
-        table.setGeometry(0,0,800,500)
-        table.setHorizontalHeaderLabels([self.ser1.name, self.ser2.name])
-        table.setVerticalHeaderLabels([attribute for attribute in self.ser1._attribs])
-        # Load table with attributes
-        for row in range(len(ser1atts)):
-            header = table.verticalHeaderItem(row).text()
-            # Series 1
-            tableItem = QtGui.QTableWidgetItem( ser1atts[header] )
-            table.setItem(row,0,tableItem)
-            # Series 2
-            tableItem = QtGui.QTableWidgetItem( ser2atts[header] )
-            table.setItem(row,1,tableItem)
-#         item.isSelected()
-#         setBackground(brush)
-#         table.setCurrentCell(r,c,comm) # <-- use to pick only 1 at a time
-#         table.selectedItems()
+        # Create table
+        self.table = seriesAttributeTable(parent=parent, s1atts=ser1atts, s2atts=ser2atts)
+        
+        # Output
+        self.goButton = QtGui.QPushButton(parent)
+        self.goButton.setText('Continue')
+        self.goButton.setGeometry(300,500,200,100)
+        self.goButton.clicked.connect(self.go)
+        
+        self.goButton.show()
         self.frame.show()
         
-        
-        
-        
-        
-         
-
-        
+    def go(self):
+        items = self.table.selectedItems()
+        if len(items) == 86:
+            print('Output Series Attributes')
+            self.goButton.hide()
+            self.frame.hide()
+            for item in items:
+                print(item.type)
+        else:
+            error = QtGui.QMessageBox(self)
+            error.setText('Make sure attributes are selected: 1 per row')
+            error.exec_()
 def main():
     app = QtGui.QApplication(sys.argv)
     win = mainFrame()
-    welcomeButton(win) # begin chain of rmtgui stuff
+#     welcomeButton(win) # begin chain of rmtgui stuff
     sys.exit( app.exec_() )
 
 if __name__ == '__main__':
