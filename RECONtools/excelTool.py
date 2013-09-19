39f42c872d9782c11252734de9584634372dcfcf
wtrdrnkr <mdmusslewhite@gmail.com>
2013-09-19 15:59:55 +0000

    added more functions, cleaned up code

--- RECONtools/excelTool.py
+++ RECONtools/excelTool.py
@@ -1,28 +1,14 @@
 #!/usr/bin/env python
 import openpyxl
 import sys, re
-
-def writeColumns(worksheet, columns):
-    for col in range( len(columns)):
-        worksheet.cell(row=0, column=col).value = str( columns[col] )
-    
-def createWorksheet(series, protList):
-    return
-    
-    
-def importColumns(template):
+import Series
+# Gathering excel info
+def importColumns(xl_template):
+    '''Builds/returns a list of columns found in template'''
     columns = []
-    for col in range( template.ncols ):
-        columns.append( str(template.cell(0,col).value) )
+    for col in range( xl_template.ncols ):
+        columns.append( str(xl_template.cell(0,col).value) )
     return columns
-
-def buildFilterBank(list_of_expressions):
-    '''Turns a list of expression into a list of regular expressions for filtering'''
-    bank = []
-    for exp in list_of_expressions:
-        exp = processExp( exp )
-        bank.append( exp )
-    return bank
 
 def processExp(exp): #===
     # Need to cap at end? $
@@ -31,6 +17,14 @@ def processExp(exp): #===
     exp = exp.replace('?', '[a-z]') #=== "? matches any single character" (numbers included?)
     exp = exp.replace('#', '[0-9]')
     return re.compile( str(exp),re.I ) # re.I (Ignore Case)
+
+def buildFilterBank(list_of_expressions):
+    '''Turns a list of expression into a list of regular expressions for filtering'''
+    bank = []
+    for exp in list_of_expressions:
+        exp = processExp( exp )
+        bank.append( exp )
+    return bank
 
 def buildProtList(series, filterBank):
     '''Returns a list of contour names from series, specified by filterBank'''
@@ -48,3 +42,25 @@ def buildProtDict(protList):
     for prot in protList:
         protDict[prot] = []
     return protDict
+
+
+# Creating excel docs
+def writeColumns(worksheet, list_of_columns):
+    '''Writes columns to specified worksheet.'''
+    for col in range( len(list_of_columns)):
+        worksheet.cell(row=0, column=col).value = str( list_of_columns[col] )
+        
+def addProtsToSheet(worksheet, protrusion_list): #===
+    '''Adds protrusions in protrusion_list to the appropriate column in worksheet.'''
+    return
+
+def addDendriteSheet(workbook, series_name, xl_template, dendrite):
+    '''Adds a worksheet to workbook for the specific dendrite. Formatted to template.'''
+    worksheet = workbook.create_sheet( title=series_name+' '+dendrite )
+    writeColumns(worksheet, importColumns(xl_template))
+
+def addDendriteSheets(workbook, series_name, xl_template, dendrite_list):
+    '''Adds a sheet to workbook for each dendrite of dendrite_list.'''
+    for dendrite in dendrite_list:
+        addDendriteSheet(workbook, series_name, xl_template, dendrite)
+    
