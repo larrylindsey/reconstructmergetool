8034f16a11274df16ff20b5f3f927cec2ea8487d
wtrdrnkr <mdmusslewhite@gmail.com>
2013-09-19 11:07:30 +0000

    removed some functions from myWorksheet class

--- RECONtools/excelTool.py
+++ RECONtools/excelTool.py
@@ -8,39 +8,6 @@ class myWorksheet:
         self.sheet = sheet
         self.filterBank = []
         
-    def buildFilterBank(self, list_of_expressions):
-        '''Turns a list of expression into a list of regular expressions for filtering'''
-        bank = []
-        for exp in list_of_expressions:
-            exp = self.processExp( exp )
-            bank.append( exp )
-        return bank
-
-    def processExp(self, exp ): #===
-        # Need to cap at end? $
-        '''Converts filter from RECONSTRUCT format to python regexp format'''
-        exp = exp.replace('*', '.')
-        exp = exp.replace('?', '[a-z]') #=== "? matches any single character" (numbers included?)
-        exp = exp.replace('#', '[0-9]')
-        return re.compile( str(exp),re.I ) # re.I (Ignore Case)
-
-    def buildProtList(self, series, filterBank):
-        '''Returns a list of contour names from series, specified by filterBank'''
-        protList = []
-        for section in series.sections:
-            for contour in [cont.name for cont in section.contours]:
-                for exp in filterBank:
-                    if exp.match(contour) != None:
-                        protList.append(contour)
-        return list(set(protList))
-
-    def buildProtDict(self, protList):
-        '''Creates a dictionary of protrusion names, each associated with a list of objects representing data'''
-        protDict = {}
-        for prot in protList:
-            protDict[prot] = []
-        return protDict
-
     def importColumns(self, template):
         columns = []
         for col in range( template.ncols ):
@@ -50,3 +17,36 @@ class myWorksheet:
     def writeColumns(self, columns):
         for col in range( len(columns) ):
             self.sheet.write(0,col,columns[col])
+
+def buildFilterBank(list_of_expressions):
+    '''Turns a list of expression into a list of regular expressions for filtering'''
+    bank = []
+    for exp in list_of_expressions:
+        exp = processExp( exp )
+        bank.append( exp )
+    return bank
+
+def processExp(exp): #===
+    # Need to cap at end? $
+    '''Converts filter from RECONSTRUCT format to python regexp format'''
+    exp = exp.replace('*', '.')
+    exp = exp.replace('?', '[a-z]') #=== "? matches any single character" (numbers included?)
+    exp = exp.replace('#', '[0-9]')
+    return re.compile( str(exp),re.I ) # re.I (Ignore Case)
+
+def buildProtList(series, filterBank):
+    '''Returns a list of contour names from series, specified by filterBank'''
+    protList = []
+    for section in series.sections:
+        for contour in [cont.name for cont in section.contours]:
+            for exp in filterBank:
+                if exp.match(contour) != None:
+                    protList.append(contour)
+    return list(set(protList))
+
+def buildProtDict(protList):
+    '''Creates a dictionary of protrusion names, each associated with a list of objects representing data'''
+    protDict = {}
+    for prot in protList:
+        protDict[prot] = []
+    return protDict
