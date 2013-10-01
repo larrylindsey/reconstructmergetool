09233896654cb1326df185558dc8e4414d009621
wtrdrnkr <mdmusslewhite@gmail.com>
2013-10-01 10:59:50 +0000

    added getters and setters to rObject

--- RECONclasses/rObject.py
+++ RECONclasses/rObject.py
@@ -4,13 +4,27 @@ class rObject:
         self.name = name
         self.series = series
         self.tag = tag
-        self.start, self.end, self.count = series.getStartEndCount( self.name )
-        self.volume = series.getVolume( self.name )
-        self.surfacearea = series.getSurfaceArea( self.name )
-        self.flatarea =  series.getFlatArea( self.name )
+        self.start, self.end, self.count = self.popStartendCount()
+        self.volume = self.popVolume()
+        self.surfacearea = self.popSurfaceArea()
+        self.flatarea =  self.popFlatArea()
         self.children = []
         
     def __str__(self):
         return 'rObject, from series '+self.series.name+', with the name '+str(self.name)
     def returnAtts(self):
         return self.name, self.start, self.end, self.count, self.volume, self.surfacearea, self.flatarea
+    def returnChildren(self):
+        return self.children
+    def popStartendCount(self):
+        if self.series != None:
+            return self.series.getStartEndCount( self.name )
+    def popVolume(self):
+        if self.series != None:
+            return self.series.getVolume( self.name )
+    def popSurfaceArea(self):
+        if self.series != None:
+            return self.series.getSurfaceArea( self.name )
+    def popFlatArea(self):
+        if self.series != None:
+            return self.series.getFlatArea( self.name )
